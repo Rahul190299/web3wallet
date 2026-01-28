9fbc8a8be66c556635a1b652afa1048541d9534c
Rahul190299 <rahulgheware123@gmail.com>
2026-01-28 23:34:54 +0530

    Initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { Box } from "lucide-react";
+export function Navbar() {
+  return <div className="flex justify-between">
+    <div>
+        <Box/>
+        
+    </div>
+    <div>
+
+    </div>
+  </div>;
+}
